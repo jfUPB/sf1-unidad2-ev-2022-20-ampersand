daca75f2de3ed18872a694ea21060de484f20101
Emilio Jose <emilio.jimenez@upb.edu.co>
2022-09-20 08:57:20 -0500

    version1

--- Arturito.ino
+++ Arturito.ino
@@ -13,23 +13,30 @@ void task()
     WAIT_COMMANDS
   };
   static TaskStates taskState = TaskStates::INIT;
-  constexpr uint8_t led = 25;
+  constexpr uint8_t led1 = 14;
+  constexpr uint8_t led2 = 27;
+  constexpr uint8_t led3 = 26;
+  constexpr uint8_t led4 = 25;
   constexpr uint8_t button1Pin = 12;
   constexpr uint8_t button2Pin = 13;
   constexpr uint8_t button3Pin = 32;
-  constexpr uint8_t button4Pin = 33;
 
   switch (taskState)
   {
   case TaskStates::INIT:
   {
     Serial.begin(115200);
-    pinMode(led, OUTPUT);
-    digitalWrite(led, LOW);
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(led3, OUTPUT);
+    pinMode(led4, OUTPUT);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
     pinMode(button1Pin, INPUT_PULLUP);
     pinMode(button2Pin, INPUT_PULLUP);
     pinMode(button3Pin, INPUT_PULLUP);
-    pinMode(button4Pin, INPUT_PULLUP);
     taskState = TaskStates::WAIT_COMMANDS;
     break;
   }
@@ -38,26 +45,64 @@ void task()
     if (Serial.available() > 0)
     {
       String command = Serial.readStringUntil('\n');
-      if (command == "ledON")
+      if (command == "led1ON")
       {
-        digitalWrite(led, HIGH);
+        digitalWrite(led1, HIGH);
       }
-      else if (command == "ledOFF")
+      else if (command == "led1OFF")
       {
-        digitalWrite(led, LOW);
+        digitalWrite(led1, LOW);
+      }
+      else if (command == "led2ON")
+      {
+        digitalWrite(led2, HIGH);
+      }
+      else if (command == "led2OFF")
+      {
+        digitalWrite(led2, LOW);
+      }
+      else if (command == "led3ON")
+      {
+        digitalWrite(led3, HIGH);
+      }
+      else if (command == "led3OFF")
+      {
+        digitalWrite(led3, LOW);
       }
       else if (command == "readBUTTONS")
       {
-
-        Serial.print("btn1: ");
-        Serial.print(btnState(digitalRead(button1Pin)).c_str());
-        Serial.print(" btn2: ");
-        Serial.print(btnState(digitalRead(button2Pin)).c_str());
-        Serial.print(" btn3: ");
-        Serial.print(btnState(digitalRead(button3Pin)).c_str());
-        Serial.print(" btn4: ");
-        Serial.print(btnState(digitalRead(button4Pin)).c_str());
-        Serial.print('\n');
+        if(digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == HIGH)
+        {
+            Serial.print("0,0,0\n");
+        }
+        else if(digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == HIGH)
+        {
+            Serial.print("1,0,0\n");
+        }
+        else if(digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == HIGH)
+        {
+            Serial.print("0,1,0\n");
+        }
+        else if(digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == LOW)
+        {
+            Serial.print("0,0,1\n");
+        }
+        else if(digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == LOW)
+        {
+            Serial.print("0,1,1\n");
+        }
+        else if(digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == HIGH && digitalRead(button1Pin) == LOW)
+        {
+            Serial.print("1,0,1\n");
+        }
+        else if(digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == HIGH)
+        {
+            Serial.print("1,1,0\n");
+        }
+        else if(digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == LOW && digitalRead(button1Pin) == LOW)
+        {
+            Serial.print("1,1,1\n");
+        }
       }
     }
     break;
